25df19caba866168000df049107a29beee5328b1
José Luiz <zeluizjsantos@gmail.com>
2025-07-29 20:19:28 -0300

    Configuração de conexão com banco de dados

---
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,38 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Register the database context with dependency injection
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options =>
+    options.UseMySql(connection, new MySqlServerVersion(new Version(8, 4, 0))));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(
+    C => C.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "GeekShopping Product API",
+        Version = "v1",
+        Description = "API for managing products in the GeekShopping application"
+    })
+    );
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
