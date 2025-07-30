e6a7e661ee4253f7c125ebeb484fcae7d498da49
José Luiz <zeluizjsantos@gmail.com>
2025-07-30 19:44:27 -0300

    Limpeza e adição de .gitignore

---
+++ GeekShopping/GeekShopping.ProductAPI/Config/MappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace GeekShopping.ProductAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config => {
+                config.CreateMap<Model.Product, Data.ValueObjects.ProductVO>();
+                config.CreateMap< Data.ValueObjects.ProductVO, Model.Product>();
+            });
+            return mappingConfig;
+        }
+    }
+}
--- GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,16 +1,27 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
 // Register the database context with dependency injection
 var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
 builder.Services.AddDbContext<MySQLContext>(options =>
     options.UseMySql(connection, new MySqlServerVersion(new Version(8, 4, 0))));
+
+// Register AutoMapper with the mapping configuration
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+// Register the DbContext with dependency injection
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+// Register the product repository
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(
