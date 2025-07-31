470458b5cdf675d488bad85a1e8a7d1f3881a5eb
José Luiz <zeluizjsantos@gmail.com>
2025-07-31 18:02:38 -0300

    Fazendo carga de products no banco

--- GeekShopping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
+++ GeekShopping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
@@ -28,7 +28,7 @@ namespace GeekShopping.ProductAPI.Repository
         async Task<ProductVO> IProductRepository.FindById(long id)
         {
             Product product = await _context.Products.Where(p => p.Id == id)
-                .FirstOrDefaultAsync();
+                .FirstOrDefaultAsync() ?? new Product();
             return _mapper.Map<ProductVO>(product);
         }
 
@@ -53,7 +53,7 @@ namespace GeekShopping.ProductAPI.Repository
             try
             {
                 Product product = await _context.Products.Where(p => p.Id == id)
-                    .FirstOrDefaultAsync();
+                    .FirstOrDefaultAsync() ?? new Product();
                 if (product == null) return false;
                 _context.Products.Remove(product);
                 await _context.SaveChangesAsync();
